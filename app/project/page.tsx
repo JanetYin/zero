5650d90fa589c16ce68a6eceed15d69ab3ef34f9
Yun37 <yinzhenyun9@gmail.com>
2024-03-31 19:54:38 +0100

    up

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,7 +1,3 @@
-// import { ExternalLink } from 'lucide-react'
-// import Comment from '@/components/Comment'
-// import data from './index.json'
-
 export default function FriendLayout() {
   return (
     <div className="page project">
@@ -9,30 +5,5 @@ export default function FriendLayout() {
         <p style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '20px' }}>Project</p>
       </div>
     </div>
-    // <div className="page project">
-    //   <ul>
-    //     {data.map((item) => {
-    //       return (
-    //         <li key={item.name} className="mb-10 leading-relaxed">
-    //           <a className="link text-xl" href={item.code} target="_blank" rel="noopener noreferrer">
-    //             {item.name}
-    //             <ExternalLink className="inline-block h-5 w-5 -translate-y-0.5 transform" />
-    //           </a>
-    //           <p>{item.description}</p>
-    //         </li>
-    //       )
-    //     })}
-    //   </ul>
-    //   <Comment term="项目" />
-    // </div>
-
   )
 }
-// [
-//   {
-//     "name": "Zero",
-//     "code": "https://github.com/chanshiyucx/zero",
-//     "demo": "https://chanshiyu.com",
-//     "description": "蝉時雨开发的博客主题。 "
-//   }
-// ]
