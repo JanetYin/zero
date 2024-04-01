003b8276b7a0f2b145410b11c50dfcd96d8c0266
Zhenyun Yin <145857496+JanetYin@users.noreply.github.com>
2024-04-01 01:23:29 +0100

    Update page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,23 +1,37 @@
-import { allPosts } from 'contentlayer/generated'
+import { allPosts,allNotes } from 'contentlayer/generated'
 import { format } from 'date-fns'
 import { Bookmark, Calendar, Tag } from 'lucide-react'
 import { notFound } from 'next/navigation'
 import Comment from '@/components/Comment'
 import MDX from '@/components/MDX'
 
-export const generateStaticParams = async () => allPosts.map((post) => ({ slug: post.title }))
+export const generateStaticParams = async () => {
+  const postSlugs = allPosts.map((post) => ({
+    params: { slug: post.title },
+    type: 'post',
+  }));
+  const noteSlugs = allNotes.map((note) => ({
+    params: { slug: note.title },
+    type: 'note',
+  }));
+  return [...postSlugs, ...noteSlugs];
+};
+
 
 export const generateMetadata = async ({ params }: { params: { slug: string } }) => {
-  const post = allPosts.find((post) => post.title === decodeURIComponent(params.slug))
+  const item = allPosts.find((post) => post.title === decodeURIComponent(params.slug)) ||
+               allNotes.find((note) => note.title === decodeURIComponent(params.slug));
+
   return {
-    title: `${post?.title} - 云影`,
-    description: post?.description,
-    keywords: post?.tags.join(','),
+    title: `${item?.title} - 云影`,
+    description: item?.description,
+    keywords: item?.tags.join(','),
   }
 }
 
 export default function PostLayout({ params }: { params: { slug: string } }) {
-  const post = allPosts.find((post) => post.title === decodeURIComponent(params.slug))
+  const post = allPosts.find((post) => post.title === decodeURIComponent(params.slug)) ||
+               allNotes.find((note) => note.title === decodeURIComponent(params.slug));
   if (!post) {
     return notFound()
   }
